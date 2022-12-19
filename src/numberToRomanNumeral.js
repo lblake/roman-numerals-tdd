3e72d952eb453d03aee0936d6e0fc2132c2bfdd2
lblake <treleven.lloyd@gmail.com>
2022-12-19 16:06:01 +0000

    refactored code to use while loop instead of if/else if block

--- src/numberToRomanNumeral.js
+++ src/numberToRomanNumeral.js
@@ -1,13 +1,16 @@
 const convertNumberToRomanNumeral = (integer) => {
-  if (integer === 1) {
-    return 'I';
-  } else if (integer === 2) {
-    return 'II';
-  } else if (integer === 3) {
-    return 'III';
-  } else if (integer === 4) {
-    return 'IV';
+  let romanNumeral = '';
+
+  while (integer > 0) {
+    if (integer < 4) {
+      romanNumeral += 'I';
+      integer -= 1;
+    } else if (integer == 4) {
+      romanNumeral += 'IV';
+      integer -= 4;
+    }
   }
+  return romanNumeral;
 };
 
 module.exports = convertNumberToRomanNumeral;
