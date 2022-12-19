40e351c0b051bd8c4c82c75dd2a7c1967220bd65
lblake <treleven.lloyd@gmail.com>
2022-12-19 15:00:03 +0000

    added code to fix failing test for number 4 to roman number

--- src/numberToRomanNumeral.js
+++ src/numberToRomanNumeral.js
@@ -1,12 +1,13 @@
 const convertNumberToRomanNumeral = (integer) => {
   if (integer === 1) {
     return 'I';
-      } else if (integer === 2) {
-        return "II";
-      } else if (integer === 3) {
-        return "III";
-      } 
-  
-    }
+  } else if (integer === 2) {
+    return 'II';
+  } else if (integer === 3) {
+    return 'III';
+  } else if (integer === 4) {
+    return 'IIII';
+  }
+};
 
 module.exports = convertNumberToRomanNumeral;
