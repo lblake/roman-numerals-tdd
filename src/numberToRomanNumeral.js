c9ee98e91f56e26bc40127e1724ee90c20ee170d
lblake <treleven.lloyd@gmail.com>
2022-12-19 14:51:33 +0000

    added test and code for number 3 to roman number also refactored code to if/else if block

--- src/numberToRomanNumeral.js
+++ src/numberToRomanNumeral.js
@@ -1,11 +1,12 @@
 const convertNumberToRomanNumeral = (integer) => {
-    if(integer === 1){ 
-        return 'I';
-    }else{
-        return 'II'
-
+  if (integer === 1) {
+    return 'I';
+      } else if (integer === 2) {
+        return "II";
+      } else if (integer === 3) {
+        return "III";
+      }
+  
     }
-};
 
-
-module.exports = convertNumberToRomanNumeral
+module.exports = convertNumberToRomanNumeral;
--- src/numberToRomanNumeral.test.js
+++ src/numberToRomanNumeral.test.js
@@ -1,10 +1,13 @@
 const convertToRomanNumeral = require('./numberToRomanNumeral');
 
-describe('Converts an integer to a Roman Number', () => {
+describe('Converts an integer to a roman number', () => {
   test('Convert the number 1 to I', () => {
     expect(convertToRomanNumeral(1)).toBe('I');
   });
   test('Convert the number 2 to II', () => {
     expect(convertToRomanNumeral(2)).toBe('II');
+  });
+  test('Convert the number 3 to III', () => {
+    expect(convertToRomanNumeral(3)).toBe('III');
   });
 });
