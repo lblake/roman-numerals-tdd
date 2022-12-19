5b8fed9a81c81203d1ec4b2e42435450888000bd
lblake <treleven.lloyd@gmail.com>
2022-12-19 20:44:29 +0000

    refactored tests to use parameterised data passed into a single test function

--- src/numberToRomanNumeral.js
+++ src/numberToRomanNumeral.js
@@ -8,13 +8,13 @@ const convertNumberToRomanNumeral = (integer) => {
     } else if (integer == 4) {
       romanNumeral += 'IV';
       integer -= 4;
-    } else if (integer === 5) {
+    } else if (integer >= 5 && integer < 9) {
       romanNumeral += 'V';
       integer -= 5;
     } else if (integer === 9) {
       romanNumeral += 'IX';
       integer -= 9;
-    } else if (integer === 10) {
+    } else if (integer >= 10) {
       romanNumeral += 'X';
       integer -= 10;
     } else {
--- src/numberToRomanNumeral.test.js
+++ src/numberToRomanNumeral.test.js
@@ -1,29 +1,17 @@
 const convertToRomanNumeral = require('./numberToRomanNumeral');
 
 describe('Converts an integer to a roman number', () => {
-  test('Convert the number 1 to I', () => {
-    expect(convertToRomanNumeral(1)).toBe('I');
-  });
-  test('Convert the number 2 to II', () => {
-    expect(convertToRomanNumeral(2)).toBe('II');
-  });
-  test('Convert the number 3 to III', () => {
-    expect(convertToRomanNumeral(3)).toBe('III');
-  });
-
-  test('Convert the number 4 to IV', () => {
-    expect(convertToRomanNumeral(4)).toBe('IV');
-  });
-
-  test('Convert the number 5 to V', () => {
-    expect(convertToRomanNumeral(5)).toBe('V');
-  });
- 
-  test('Convert the number 9 to IX', () => {
-    expect(convertToRomanNumeral(9)).toBe('IX');
-  });
-
-  test('Convert the number 10 to X', () => {
-    expect(convertToRomanNumeral(10)).toBe('X');
+  it.each([
+    [1, 'I'],
+    [2, 'II'],
+    [3, 'III'],
+    [4, 'IV'],
+    [5, 'V'],
+    [9, 'IX'],
+    [10, 'X'],
+    [11, 'XI'],
+    [16, 'XVI'],
+  ])(`should convert the number %i, to roman numeral %s`, (number, result) => {
+    expect(convertToRomanNumeral(number)).toBe(result);
   });
 });
